b081c9e0c01006d73d464e1c2eb66ae7eb6642ea
Vladislav <vlad_moryakov_prog@mail.ru>
2021-09-26 15:56:24 +0300

    pizdec

--- controllers/ChartController.py
+++ controllers/ChartController.py
@@ -1,13 +1,16 @@
 import json
 import sys
+import asyncio
+import threading
 
-from PyQt5 import QtWidgets
-
-from views.MainView import MainView, Loader, GridDrawer, LineChartDrawer
-
-from repository.ChartRepository import ChartRepository
 from models.Candle import Candle
 from models.Line import Line
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QPainter, QPen, QPixmap
+from views.MainView import GridDrawer, LineChartDrawer, Loader, MainView
+
+from repository.ChartRepository import ChartRepository
+import time
 
 
 class VertexesFactory:
@@ -34,18 +37,56 @@ class ChartController:
 
         self._view = MainView()
 
-        # self._states = {
-        #     "loader": Loader(self._view.painter),
-        #     "connectionChart": ChartController().getChartData,
-        #     "gridDrawer": GridDrawer(self._view.painter),
-        #     "vertexDrawer": LineChartDrawer(self._view.painter),
-        # }
+        # иницилизация пиксмапа
+        self._view.createPainter()
+
+        self._states = {
+            "loader": Loader(self._view.painter),
+            "connectionChart": self.getChartData,
+            "gridDrawer": GridDrawer(self._view.painter).draw,
+            "vertexDrawer": LineChartDrawer(self._view.painter),
+        }
+
+        # поток отрисовки графика
+        thread_chart_drawer = threading.Thread(target=self.runChart, args=())
+        thread_chart_drawer.start()
+
+        # логика отрисовки
+        self._view.closePainter()
+
+        # установка пиксмапа
+        self._view.setPainter()
+
+    def runChart(self):
+
+        self.data = None
+
+        self._view.clearPainter()
+
+        for state in self._states.keys():
+
+            action = self._states[state]
+
+            if state == "loader":
+                pass
+
+            if state == "connectionChart":
+                self.data = action()
+
+            if state == "gridDrawer":
+                if not self.data:
+                    print("data is None")
+                    return
+
+                action(self.data)
+
+            if state == "vertexDrawer":
+                pass
 
     def getChartData(self):
-        data = self.chartRepository.getRemoteData()
-        return self.vertexesFactory.createVertexes(json.loads(data))
+        data = self._chartRepository.getRemoteData()
+        return self._vertexesFactory.createVertexes(json.loads(data))
 
     def run(self):
         self._view.show()
         return self._app.exec_()
-
--- views/MainView.py
+++ views/MainView.py
@@ -9,21 +9,32 @@ from res.dist.templates.Chart import Ui_MainWindow as Chart
 # from controllers.ChartController import ChartController
 
 class GridDrawer:
-    def __init__(self, canvas) -> None:
+    def __init__(self, painter: QPainter) -> None:
+        self.painter = painter
+        # print(self.painter.device().height())
+        # print(self.painter.device().width())
+
+    def draw(self, data):
         pass
 
+
 class LineChartDrawer(GridDrawer):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, painter) -> None:
+        super().__init__(painter)
 
 
 class CandleChartDrawer(LineChartDrawer):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, painter) -> None:
+        super().__init__(painter)
     
 class Loader:
-    def __init__(self) -> None:
+    def __init__(self, painter) -> None:
+        self.painter = painter
         pass
+
+    def runLoader(self):
+        self.painter
+
 
 class MainView(QMainWindow):
 
@@ -33,20 +44,19 @@ class MainView(QMainWindow):
         self.chart = Chart()
         self.chart.setupUi(self)
 
-        pixmap = QPixmap(self.chart.drawLabel.size())
-        pixmap.fill(QtCore.Qt.transparent)
-        self.painter = QPainter(pixmap)
+    def createPainter(self):
+        self.pixmap = QPixmap(self.chart.drawLabel.size())
+        self.pixmap.fill(QtCore.Qt.transparent)
+        self.painter = QPainter(self.pixmap)
+
+    def closePainter(self):
         pen = QPen(QtCore.Qt.red, 3)
         self.painter.setPen(pen)
         self.painter.drawLine(10, 10, 50, 50)
         self.painter.end()
-        self.chart.drawLabel.setPixmap(pixmap)
 
+    def setPainter(self):
+        self.chart.drawLabel.setPixmap(self.pixmap)
 
-        self._drawChart()
-
-    def _drawChart(self):
-        # vertexes = self.states["connectionChart"]();
-        # print(vertexes)
-
-        print("Ща рисовать будем")
+    def clearPainter(self):
+        self.chart.drawLabel.clear()
