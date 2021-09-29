8be9490c03703cf02f928895de4aaa72e66d4354
tse-egor <82537848+tse-egor@users.noreply.github.com>
2021-09-29 13:34:24 +0300

    print correct data

--- other-widgets/news-widget/parser/TradingView.py
+++ other-widgets/news-widget/parser/TradingView.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import lxml
+import cchardet
 
 
 class Parser:
@@ -11,7 +13,8 @@ class Parser:
         self.HOST = 'https://ru.tradingview.com'
 
     def get_html_text(self, url):
-        page = requests.get(url, headers=self.HEADERS)
+        requests_session = requests.Session()
+        page = requests_session.get(url, headers=self.HEADERS)
         html_text = page.text
         return html_text
 
@@ -19,14 +22,18 @@ class Parser:
         pass
 
     def find_all_cards(self):
-        soup = BeautifulSoup(self.get_html_text(url=self.URL), 'html.parser')
+        soup = BeautifulSoup(self.get_html_text(url=self.URL), 'lxml')
         items = soup.find_all('div', class_='tv-feed-layout__card-item')
-        for item in items[4]:
-            title = ''
-            name = ''
-            link = ''
-            image = ''
-        print(items)
+        for item in items[:1]:
+            article_title = soup.find('a', class_='tv-widget-idea__title').get_text(strip=True)
+            crypto_name = soup.find('a', class_='tv-widget-idea__symbol').get_text(strip=True)
+            article_link = self.HOST + item.find('a', class_='tv-widget-idea__title').get('href')
+            article_image_link = soup.find('img', 'tv-widget-idea__cover').get('data-src')
+            print(article_title)
+            print(crypto_name)
+            print(article_link)
+            print(article_image_link)
+
 
 parser = Parser()
 parser.find_all_cards()
